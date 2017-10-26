9283609edb783bda4473ae4722f83393f3be397a
Zhusupbekova, May <mzhusupbekova@lcs-mba3239.lcs.on.ca>
2017-10-26 10:25:25 -0400

    Got small animation running

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -28,6 +28,60 @@ let canvas = Canvas(width: 400, height: 300)
 
 
 
+import Foundation
+
+class Sketch : NSObject {
+    
+    // NOTE: Every sketch must contain an object of type Canvas named 'canvas'
+    //       Therefore, the line immediately below must always be present.
+    let canvas : Canvas
+    
+    // Position of circle
+    var x : Int
+    
+    // This function runs once
+    override init() {
+        
+        // Create canvas object – specify size
+        canvas = Canvas(width: 500, height: 500)
+        
+        // Set starting position
+        x = 250
+        
+    }
+    
+    // Runs in a loop, forever, to create the animated effect
+    func draw() {
+        
+        // Change position
+        x += 1
+        
+        // Draw an ellipse in the middle of the canvas
+        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*:
  ## Use source control
  To keep your work organized, and receive feedback, source control is a must.
